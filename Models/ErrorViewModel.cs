226ac7d1926e82046b87aa005ea426ddedeea342
Camilo <blanquicetcar@gmail.com>
2022-02-13 11:24:25 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiPrimerProyectoASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
